abc323262f754192720ff56726ecb8f0305ae0c5
Adwaid-K-S <106295574+Adwaid-K-S@users.noreply.github.com>
2022-07-14 00:14:19 +0530

    Add files via upload

---
+++ signup-sql.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Community v13.1.7 (64 bit)
+MySQL - 10.4.24-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `signup` (
+	`id` int (11),
+	`username` varchar (90),
+	`email` varchar (90),
+	`password` varchar (30)
+); 
+insert into `signup` (`id`, `username`, `email`, `password`) values('1','A','damodaran@gmail.com','456456');
